fe1ef28b80a20193437521511e07d5743c100d2b
mind <junhek2525@gmail.com>
2025-12-04 15:54:09 +0900

    [Add] add 2

--- Assets/Scripts/Entity/Enemy/Enemy_Animation.cs
+++ Assets/Scripts/Entity/Enemy/Enemy_Animation.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public abstract class Enemy_Animation
 {
+
+    protected Animator anim;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
