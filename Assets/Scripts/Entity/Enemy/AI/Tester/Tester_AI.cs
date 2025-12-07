0747144e2414248633aebcc5832784797a83b4de
ddddsssswwww <junhek2525@gmail.com>
2025-12-07 19:56:52 +0900

    [Add] add3

--- Assets/Scripts/Entity/Enemy/AI/Tester/Tester_AI.cs
+++ Assets/Scripts/Entity/Enemy/AI/Tester/Tester_AI.cs
@@ -1,9 +1,7 @@
 using UnityEngine;
 
-public class Enemy : Entity
+public class Tester_AI : MonoBehaviour
 {
-
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
